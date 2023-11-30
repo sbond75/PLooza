9861fb6e7f511bf67101e6bc1001c149812f4bf9
sbond75 <43617712+sbond75@users.noreply.github.com>
2023-11-30 15:01:53 -0500

    tests updated

--- examples/tests/runTests.sh
+++ examples/tests/runTests.sh
@@ -2,6 +2,7 @@
 #! nix-shell -i bash ../../compiler/shell.nix
 
 verbose="$1"
+# Environment variables that can be provided: `debug=1` to debug this script.
 
 if [ "$verbose" == "1" ]; then
     cmd="diff -y"
@@ -11,14 +12,42 @@ fi
 SCRIPT_DIR="$(dirname "${BASH_SOURCE[0]}")"
 
 # https://stackoverflow.com/questions/4638874/how-to-loop-through-a-directory-recursively-to-delete-files-with-certain-extensi
-for f in $(find "$SCRIPT_DIR" -name '*.txt'); do
+for f in $(find "$SCRIPT_DIR" -name '*.txt' ! -name '*.stdin.txt'); do
     fOutput="$f"
     f="$SCRIPT_DIR/../$(realpath --relative-to "$SCRIPT_DIR" "$f")"
-    output="$(diff "$fOutput" <(python3 "$SCRIPT_DIR/../../compiler/main.py" "$f" 2>&1))"
+
+    path=$(dirname -- "$fOutput")
+    filename=$(basename -- "$fOutput")
+    extension="${filename##*.}"
+    filename="${filename%.*}"
+    if [ "$debug" == "1" ]; then
+	echo "$filename"
+    fi
+    stdinFile="$path/${filename}.stdin.txt"
+    # if [ "$debug" == "1" ]; then
+    # 	if [ -e "$stdinFile" ]; then
+    # 	    echo "stdin file exists: $stdinFile"
+    # 	else
+    # 	    echo "stdin file doesn't exist: $stdinFile"
+    # 	fi
+    # fi
+    
+    if [ -e "$stdinFile" ]; then
+	# Use stdin from this file
+	output="$(diff "$fOutput" <(cat "$stdinFile" | python3 "$SCRIPT_DIR/../../compiler/main.py" "$f" 2>&1))"
+    else
+	output="$(diff "$fOutput" <(python3 "$SCRIPT_DIR/../../compiler/main.py" "$f" 2>&1))"
+    fi
+    
     if [ ! -z "$output" ]; then
 	echo "Test $fOutput failed:"
 	if [ "$verbose" == "1" ]; then
-	    diff -y "$fOutput" <(python3 "$SCRIPT_DIR/../../compiler/main.py" "$f" 2>&1)
+	    if [ -e "$stdinFile" ]; then
+		# Use stdin from this file
+		diff -y "$fOutput" <(cat "$stdinFile" | python3 "$SCRIPT_DIR/../../compiler/main.py" "$f" 2>&1)
+	    else
+		diff -y "$fOutput" <(python3 "$SCRIPT_DIR/../../compiler/main.py" "$f" 2>&1)
+	    fi
 	else
 	    echo "$output"
 	fi
